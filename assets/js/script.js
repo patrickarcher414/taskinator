e165d3fe85ee756e7e1f42c0745a9f0f562ed6e3
patrickarcher414 <patrickarcher414@gmail.com>
2022-03-20 23:36:35 -0500

    add task type infor to li

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,17 @@ var taskToDo = document.querySelector("#tasks-to-do")
 
 function addTaskHandler(event) {
     event.preventDefault()
+    var taskNameInput = document.querySelector("input[name='task-name']").value
+    var taskTypeInput = document.querySelector("select[name='task-type']").value
     var taskItem = document.createElement("li")
     taskItem.className = "task-item"
-    taskItem.innerHTML = "this is a new task"
+
+    var taskInfoEl = document.createElement("div")
+    taskInfoEl.className = "task-info"
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+
+
+    taskItem.appendChild(taskInfoEl)
     taskToDo.appendChild(taskItem)
 }
 
